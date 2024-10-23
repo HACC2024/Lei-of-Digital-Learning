085d706b4d707b264ce71fd9d1978494264ddd00
Ed White <ew@spyhi.com>
2024-10-22 21:10:24 -1000

    Added route to verify test db liveness

--- api/camdn/main.py
+++ api/camdn/main.py
@@ -1,3 +1,5 @@
+import psycopg
+
 from os import environ
 
 from typing import Union
@@ -24,9 +26,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class LoginRequest(BaseModel):
     username: str
     password: str
+
 
 @app.get("/")
 def read_root():
@@ -37,9 +41,27 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/login")
 def login(request: LoginRequest):
     if request.username == "admin" and request.password == "admin":
         return {"token": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u"}
     else:
         raise HTTPException(status_code=401)
+
+
+@app.get("/testdb")
+def test_db():
+    # Putting all this in here is not best practice, just for demo
+    conn = psycopg.connect(
+        dbname=environ["POSTGRES_USER"],
+        user=environ["POSTGRES_USER"],
+        password=environ["POSTGRES_PASSWORD"],
+        host=environ["POSTGRES_HOST"],
+        port=environ["POSTGRES_PORT"],
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT version()")
+    pg_version = cur.fetchone()
+    return {"pg_version": pg_version}
