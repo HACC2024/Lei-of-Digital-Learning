479d2699014baa6db8f9b279d0163cf0009ce6bd
Ed White <ew@spyhi.com>
2024-10-23 22:52:55 -1000

    Remove unused imports and restore api/env liveliness check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import Login from "./components/Login.tsx";
@@ -27,11 +25,10 @@ function App() {
 
   return (
     <>
-
       <div className="main-content">
-        <Login />  
-      </div>      
-
+        <Login />
+        <h1>{greet}</h1>
+      </div>
     </>
   );
 }
